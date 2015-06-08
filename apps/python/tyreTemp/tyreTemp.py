fce44649044a2700aca1218044aa107e399977b4
Scott Davey <me@sdavey.uk>
2015-06-08 15:14:37 +0100

    Updates method calls for latest V of Assetto Corsa
    Provided Python DOC is out of date regarding tyre temps, not provides
    core and surface temps seperately

--- apps/python/tyreTemp/tyreTemp.py
+++ apps/python/tyreTemp/tyreTemp.py
@@ -6,7 +6,7 @@ import sys
 import ac
 import acsys
 
-APP_NAME = "Tyre Temps"
+APP_NAME = "tyreTemp"
 tyre1Label = tyre2Label = tyre3Label = tyre4Label = 0
 
 def acMain(ac_version):
@@ -14,22 +14,27 @@ def acMain(ac_version):
     ac.setSize(appWindow, 200, 200)
     ac.log("==TYRE TEMPS BEGIN==")
     setupUI(appWindow)
+    ac.addRenderCallback(appWindow, onFormRender)
     return APP_NAME
 
 def setupUI(appWindow):
     global tyre1Label, tyre2Label, tyre3Label, tyre4Label
-    tyre1Label = ac.addLabel(appWindow, "Tyre 1: 0");
-    tyre2Label = ac.addLabel(appWindow, "Tyre 2: 0");
-    tyre3Label = ac.addLabel(appWindow, "Tyre 3: 0");
-    tyre4Label = ac.addLabel(appWindow, "Tyre 4: 0");
+    tyre1Label = ac.addLabel(appWindow, "FL: 0");
+    tyre2Label = ac.addLabel(appWindow, "FR: 0");
+    tyre3Label = ac.addLabel(appWindow, "RL: 0");
+    tyre4Label = ac.addLabel(appWindow, "RR: 0");
     ac.setPosition(tyre1Label, 3, 30)
-    ac.setPosition(tyre2Label, 3, 40)
-    ac.setPosition(tyre3Label, 3, 50)
-    ac.setPosition(tyre4Label, 3, 60)
+    ac.setPosition(tyre2Label, 3, 50)
+    ac.setPosition(tyre3Label, 3, 70)
+    ac.setPosition(tyre4Label, 3, 90)
 
-def acUpdate(deltaT):
-    temps = ac.getCarState(0, acsys.CS.ThermalState)
-    ac.setText(tyre1Label, "Tyre 1: {}".format(temps[0]))
-    ac.setText(tyre2Label, "Tyre 2: {}".format(temps[1]))
-    ac.setText(tyre3Label, "Tyre 3: {}".format(temps[2]))
-    ac.setText(tyre4Label, "Tyre 4: {}".format(temps[3]))
+def onFormRender(deltaT):
+    global tyre1Label, tyre2Label, tyre3Label, tyre4Label
+    try:        
+        x,y,z,w = ac.getCarState(0, acsys.CS.CurrentTyresCoreTemp)
+        ac.setText(tyre1Label, "FL: {}".format(round(x,1)))
+        ac.setText(tyre2Label, "FR: {}".format(round(y,1)))
+        ac.setText(tyre3Label, "RL: {}".format(round(z,1)))
+        ac.setText(tyre4Label, "RR: {}".format(round(w,1)))
+    except Exception as e:
+        ac.log("TyreTemp: Error in function onFormRender: {}".format(e))
